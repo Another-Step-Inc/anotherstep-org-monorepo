766d786a947c38b4db991c2caf9264934c2b245f
Gregory Rothstein <grothstein@anotherstep.org>
2026-07-06 16:36:42 -0400

    Query content and layout from wordpress
    - Refactor the query to stay DRY for:
    - About Us
    - Advocacy
    - Contact Us
    - Home
    - Add badge Style to Block Resolver
    - Add badge style to Hero
    - Change layouts for some configurations
    - Created a lib folder with wp.js to reuse queries and fetch functions
    - Add lib folder to package.json file

---
+++ packages/ui/src/lib/wp.js
@@ -0,0 +1,57 @@
+const WP_URL = "http://host.docker.internal:8050/graphql";
+
+// Reusable Editor Blocks Fragment
+export const EDITOR_BLOCKS_FRAGMENT = `
+    editorBlocks {
+        name
+        renderedHtml
+        ... on AnotherstepPageHeroSection {
+            attributes {
+                layoutType
+                heroTitle
+                heroDescription
+                heroImageUrl
+                heroImageId
+                imageDecoration
+                badgeText
+                badgeStyle
+                hasBadge
+                quoteText
+                statsText
+                statsNumber
+                ctaStyle
+                extraDivText
+                btn1Text
+                btn1Url
+                btn2Text
+                btn2Url
+            }
+        }
+        ... on CoreImage {
+            attributes {
+                url
+                alt
+                caption
+            }
+        }
+    }
+`;
+
+/**
+ * Reusable wrapper for executing GraphQL queries against WordPress
+ */
+export async function wpFetch(query, variables = {}) {
+    try {
+        const response = await fetch(WP_URL, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({query, variables}),
+        });
+
+        const result = await response.json();
+        return result.data;
+    } catch (error) {
+        console.error("WP GraphQL Fetch Error:", error);
+        return null;
+    }
+}
